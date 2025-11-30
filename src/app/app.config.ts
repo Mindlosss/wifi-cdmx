b5051b8da5db54a8b80631693c27f3fc35b46567
Mindlosss <projectal4n@gmail.com>
2025-11-30 17:38:14 -0600

    Instalación y configuración de dependencias.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({
+      projectId: "wifi-cdmx-22",
+      appId: "1:347091077124:web:bae423979eaee4f14b4d51",
+      storageBucket: "wifi-cdmx-22.firebasestorage.app",
+      apiKey: "AIzaSyDRJKubpWeXGuPBWL8eZ7u3o0TZs8bDuIg",
+      authDomain: "wifi-cdmx-22.firebaseapp.com",
+      messagingSenderId: "347091077124"
+    })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
